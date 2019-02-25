70f01f251a0769781eb3c06e82c53efddd78eade
xaviguasch <xaviguasch@gmail.com>
2019-02-25 20:13:18 +0100

    added basic driver commands

--- app.js
+++ app.js
@@ -19,5 +19,44 @@ client.connect(function (err) {
 
     const db = client.db(dbName);
 
-    client.close();
+    findDocuments(db, function () {
+        client.close();
+    });
 });
+
+const insertDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Insert some documents
+    collection.insertMany([{
+        name: "Apple",
+        score: 8,
+        review: "Great Fruit"
+    }, {
+        name: "Orange",
+        score: 6,
+        review: "Kinda sour"
+    }, {
+        name: "Banana",
+        score: 9,
+        review: "Great stuff!"
+    }], function (err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+}
+
+const findDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Find some documents
+    collection.find({}).toArray(function (err, fruits) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(fruits)
+        callback(fruits);
+    });
+}
